f4badf1eacb7864ebb37818eb9bae5204598033e
gong_i221n <ivan.chaschin0@gmail.com>
2022-02-07 23:29:42 +0300

    Создал ProfileView и подключил Outlets

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Ivan Chaschin on 07.02.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var myTextView: UITextView!
+    
+}
